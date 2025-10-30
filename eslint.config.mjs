8f7cb0178370c796e0dde4c5c9249ae7371532ad
Joe Powers <54547147+ReadySetJoe@users.noreply.github.com>
2025-10-29 23:55:30 -0400

    adds and uses prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,34 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      // Disable rules that conflict with Prettier
+      'no-multiple-empty-lines': 'off',
+      'no-trailing-spaces': 'off',
+    },
+  },
+  prettier, // This should be last to override other configs
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    // Additional ignores
+    'node_modules/**',
+    '.git/**',
   ]),
 ]);
 
